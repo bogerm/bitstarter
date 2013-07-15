12d0e16242f1e763ad49c3ba91c63f87f6d93ef0
Oleg Buklanov <buklanov@gmail.com>
2013-07-15 17:47:17 +0000

    Fixed errors

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var INDEX = "index.html";
 
 app.get('/', function(request, response) {
    var buffer = fs.readFileSync(INDEX); 
-   response.setHeader('Content-Type','text/plain');
+   response.setHeader('Content-Type','text/html');
    response.setHeader('Content-Length',buffer.length);
    response.send(buffer);
 });
